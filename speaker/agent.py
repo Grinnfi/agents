b1b11c087836898dd36b809d3138efcc2eef3563
Daniel Vitor <dvmc1234@gmail.com>
2025-06-12 11:40:16 -0300

    Fix tool directory

--- speaker/agent.py
+++ speaker/agent.py
@@ -2,25 +2,15 @@ import os
 
 # from typing import Optional
 from google.adk.agents import Agent
-from google.adk.agents.callback_context import CallbackContext
+# from google.adk.agents.callback_context import CallbackContext
+from speaker.tools.text_to_speech import text_to_speech
 from dotenv import load_dotenv
-from utils.tts import audio_to_storage
 
 load_dotenv()
 
 project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 location = os.getenv("GOOGLE_CLOUD_LOCATION")
 model_base = os.getenv("MODEL_LITE")
-
-def text_to_speach(text: str) -> str:
-    """Synthesizes speech from the input text and returns the file path.
-    Args:
-        text[str]: A string to be converted
-    Returns:
-        file_path[str]: The audio file path
-    """
-    return (audio_to_storage(text))
-
 
 root_agent = Agent(
     model=model_base, # type: ignore
@@ -31,5 +21,5 @@ root_agent = Agent(
     instruction=(
         "Talk to the user. Only if asked for audio,  use the `text_to_speach` tool to return the audio file."
     ),
-    tools=[text_to_speach]
+    tools=[text_to_speech]
 )
